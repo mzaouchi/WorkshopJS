f9bf645f8036d8811271ae2dd737dabf70381b9b
mzaouchi <mzaouchi@gmail.com>
2023-10-27 09:29:43 +0100

    first commit

---
+++ WorkshopJS.js
@@ -0,0 +1,186 @@
+// var a = "Hello JS"
+// console.log(a)
+// console.log(typeof (a))
+
+// var c = 8.4
+
+// console.log(c)
+// console.log(typeof(c))
+
+// var d = true
+
+// console.log(d)
+// console.log(typeof(d))
+
+
+// var a = [2,4,6,7,9,40000]
+
+
+// console.log(a[2])
+// console.log(a[1])
+// console.log(a[a.length-1])
+// console.log(a[0])
+
+// console.log(typeof(a))
+
+// var user = { name: "Omar", age: 31, absent: false }
+
+// console.log(user.name)
+// console.log(user.age)
+
+// console.log(user)
+
+// console.log(Object.keys(user))
+// console.log(Object.values(user))
+// console.log(Object.entries(user))
+
+// console.log(typeof(user))
+
+// var z
+
+// console.log(typeof(z))
+
+
+// var a = null
+
+// console.log(typeof(a))
+
+
+// function Somme(a,b){
+//     return a+b
+//   }
+  
+//   console.log(Somme(4,2))
+  
+//   console.log(typeof(Somme))
+
+
+//   var a = 5
+// var b = 2
+
+
+// // console.log(a+b)
+// // console.log(a-b)
+// // console.log(a*b)
+// // console.log(a/b)
+// // console.log(a%b)
+// // console.log(a**b)
+// // a = a+1
+// // console.log(a)
+// // a++
+// // console.log(a)
+// // a--
+// // console.log(a)
+// // a = a -1
+// // console.log(a)
+
+// // a+=4 //a = a +4
+// // console.log(a)
+
+// console.log(a)
+
+
+// var a = 5
+// var b = 3
+// var c = '5'
+
+// console.log(a == b)
+// console.log(a == c)
+// console.log(a === c)
+
+// console.log(a !=  c)
+// console.log(a !== c)
+
+// var a = 5
+// var b = 3
+
+
+// console.log(a>b)
+// console.log(a<b)
+// console.log(a>=b)
+// console.log(a<=b)
+
+// var a = 5
+// var b = 3
+
+
+// // console.log(a<b && a>b)
+
+// // console.log(a < b || a > b)
+
+// // console.log(a>b && false)
+
+// var a = 0
+
+
+// // if(a>=0){
+// //   console.log("Positif")
+// // } else{
+// //   console.log("Negatif")
+// // }
+
+// // if(a == 0){
+// //   console.log("Zero")
+// // }else if (a>0){
+// //   console.log("Positif")
+// // }else{
+// //   console.log("Negatif")
+// // }
+
+
+// var age = 61
+
+// if(age<18){
+//   console.log("Jeune")
+// }else if(age<=60){
+//   console.log("Adule")
+// }else{
+//   console.log("Agée")
+// }
+
+
+// var a = 8
+
+
+// switch(a){
+//   case 1 : console.log("Janvier")
+//            break 
+//   case 2: console.log("Fev")
+//            break
+//   default : console.log("Erreur")
+// }
+
+
+
+// // for(var i = 0;   i<10   ;   i++){
+// //   // console.log("Bonjour")
+// //   console.log(i+1)
+// // }
+
+// var t = [2,3,5,90,97,4,3,6]
+
+// // for(var i = 0; i < t.length; i++){
+// //   console.log(t[i])
+// // }
+
+// // for(var key in t){
+// //   console.log(t[key])
+// // }
+
+// for( var sirine of t){
+//   console.log(sirine)
+// }
+
+var t = [2,3,4]
+
+var i = 6
+
+while (i<t.length) {
+    console.log(t[i])
+    i++
+}
+console.log("Omar")
+do {
+        console.log(t[i])
+        i++
+} while (i<t.length);
